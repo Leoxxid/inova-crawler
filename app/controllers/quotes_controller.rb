d23768f0925caf672013211ab2173de5965c446d
leoxxid <leoxxid@hotmail.com>
2019-02-12 02:26:08 -0200

    Add clean cache option

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,10 @@
 # QuotesController
 class QuotesController < ApplicationController
   before_action :set_search_tag
+  before_action :set_tag
+  before_action :destroy_tag, if: -> { params[:'clean-cache'] && @tag }
 
   def show
-    @tag = SearchTag.where(name: @search_tag).first
     if @tag
       render json: @tag.quotes
     else
@@ -18,7 +19,18 @@ class QuotesController < ApplicationController
     end
   end
 
+  private
+
   def set_search_tag
     @search_tag = params[:search_tag]
+  end
+
+  def set_tag
+    @tag = SearchTag.where(name: @search_tag).first
+  end
+
+  def destroy_tag
+    @tag.destroy
+    @tag = nil
   end
 end
